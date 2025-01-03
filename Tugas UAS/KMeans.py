b1d02fbf1f234273034bd889172ae0782e609506
ums-l200220230 <rizkyzky345@gmail.com>
2025-01-04 00:44:30 +0700

    Use 2 algorithm

--- Tugas UAS/KMeans.py
+++ Tugas UAS/KMeans.py
@@ -1,28 +1,29 @@
 from metaflow import FlowSpec, step, Parameter, resources, conda_base, profile
-from sklearn.mixture import GaussianMixture
+from sklearn.cluster import KMeans
 from analyze_kmeans import top_words
 @conda_base(python="3.10.16", libraries={"scikit-learn": "1.5.2"})
-class GaussianFlow(FlowSpec):
+class KMeansFlow(FlowSpec):
     num_docs = Parameter('num-docs',help='Number of documents')
+
     @resources(memory=200)
     @step
     def start(self):
         import scale_data
         banned_words = {"rafli","rizky","gilang","pandu","joko","afrizal","farhan","chandra"}
-        docs = scale_data.load_chat(self.num_docs,banned_words=banned_words)
+        docs = scale_data.load_chat(self.num_docs, banned_words=banned_words)
         self.mtx, self.cols = scale_data.make_matrix(docs)
-        self.gmm_params = [3,4,5]
-        self.next(self.train_gmm, foreach='gmm_params')
+        self.kmeans_params = [3,4,5]
+        self.next(self.train_kmeans, foreach='kmeans_params')
     
     @resources(cpu=1, memory=200)
     @step
-    def train_gmm(self):
+    def train_kmeans(self):
         self.k = self.input
-        with profile('gmm'):
-            gmm = GaussianMixture(n_components=self.k, random_state=42,n_init=10)
+        with profile('kmeans'):
+            kmeans = KMeans(n_clusters=self.k, random_state=42,n_init=10)
             mtx_dense = self.mtx.toarray()
-            gmm.fit(mtx_dense)
-        self.clusters = gmm.predict(mtx_dense)
+            kmeans.fit(mtx_dense)
+        self.clusters = kmeans.predict(mtx_dense)
         self.next(self.analyze)
     
     @step
@@ -39,4 +40,4 @@ class GaussianFlow(FlowSpec):
     def end(self):
         pass
 if __name__ == '__main__':
-    GaussianFlow()
+    KMeansFlow()
--- Tugas UAS/scale_data.py
+++ Tugas UAS/scale_data.py
@@ -3,25 +3,25 @@ import tarfile
 from sklearn.feature_extraction.text import CountVectorizer
 
 
-def clean_chat(text):
-    cleaned_text = re.sub(r"\d+", "", text) 
-    
-    cleaned_text = re.sub(r"[^a-zA-Z.,!?;:'\"()\[\]{}\-_/ ]+", " ", cleaned_text)
-    
-    cleaned_text = cleaned_text.lower()
-    
+def clean_chat(text, banned_words=None):
+    if banned_words is None:
+        banned_words = set()
+
+    cleaned_text = re.sub(r"[^a-zA-Z.,!?;:'\"()\[\]{}\-_/ ]+", "", text)
     cleaned_text = re.sub(r"\s+", " ", cleaned_text).strip()
-    
+
+    words = cleaned_text.split()
+    cleaned_text = " ".join(word for word in words if word.lower() not in banned_words)
     return cleaned_text
 
-def load_chat (nums_docs):
-    path = "ChatGroup.tar"
-    with tarfile.open(path) as tar :
-        datafile = tar.extractfile('ChatGroup.txt')
-        content = datafile.read().decode('utf-8',errors='ignore')
-        lines = content.splitlines()[:nums_docs]
 
-        cleaned_text = [clean_chat(line) for line in lines]
+def load_chat(nums_docs, banned_words=None):
+    path = "ChatGroup.tar"
+    with tarfile.open(path) as tar:
+        datafile = tar.extractfile('ChatGroup.txt')
+        content = datafile.read().decode('utf-8', errors='ignore')
+        lines = content.splitlines()[:nums_docs]
+        cleaned_text = [clean_chat(line, banned_words=banned_words) for line in lines]
         return cleaned_text
 
     
